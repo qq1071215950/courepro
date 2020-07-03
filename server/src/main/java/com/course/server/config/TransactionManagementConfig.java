ba0459b9c9590d2f13c3776d56bc704131fd1086
黎立健 <wb-llj614947@alibaba-inc.com>
2020-07-03 17:17:28 +0800

    1配置事务 2 加入时长格式处理过滤器

---
+++ server/src/main/java/com/course/server/config/TransactionManagementConfig.java
@@ -0,0 +1,14 @@
+package com.course.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * 事务配置
+ * @author jiange
+ * @date 2020/7/3 17:11
+ */
+@EnableTransactionManagement
+@Configuration
+public class TransactionManagementConfig {
+}
--- server/src/main/java/com/course/server/service/impl/SectionServiceImpl.java
+++ server/src/main/java/com/course/server/service/impl/SectionServiceImpl.java
@@ -12,6 +12,7 @@ import com.course.server.utils.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -52,6 +53,7 @@ public class SectionServiceImpl implements SectionService {
     /**
      * 保存，id有值时更新，无值时新增
      */
+    @Transactional
     @Override
     public void save(SectionDto sectionDto) {
         Section section = CopyUtil.copy(sectionDto, Section.class);
