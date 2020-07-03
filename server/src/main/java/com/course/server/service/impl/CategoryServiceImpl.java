e5aaa46091965def852dc42a6b2b708bc44f01b9
黎立健 <wb-llj614947@alibaba-inc.com>
2020-07-03 18:07:16 +0800

    两级分类的增改删

--- server/src/main/java/com/course/server/service/impl/CategoryServiceImpl.java
+++ server/src/main/java/com/course/server/service/impl/CategoryServiceImpl.java
@@ -11,6 +11,7 @@ import com.course.server.utils.UuidUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
@@ -65,15 +66,29 @@ public class CategoryServiceImpl implements CategoryService {
     private void update(Category category) {
         categoryMapper.updateByPrimaryKey(category);
     }
-
     /**
      * 删除
      */
+    @Transactional
     @Override
     public void delete(String id) {
+        deleteChildren(id);
         categoryMapper.deleteByPrimaryKey(id);
     }
 
+    /**
+     * 删除子分类
+     * @param id
+     */
+    public void deleteChildren(String id) {
+        Category category = categoryMapper.selectByPrimaryKey(id);
+        if ("00000000".equals(category.getParent())) {
+            // 如果是一级分类，需要删除其下的二级分类
+            CategoryExample example = new CategoryExample();
+            example.createCriteria().andParentEqualTo(category.getId());
+            categoryMapper.deleteByExample(example);
+        }
+    }
     /**
      * 列表查询
      */
