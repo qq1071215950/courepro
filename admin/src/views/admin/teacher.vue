911fd8f38a2973cb8570c3d7e06a2856e5ffc079
黎立健 <wb-llj614947@alibaba-inc.com>
2020-07-07 18:11:10 +0800

    文件上传组件的制作

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -80,11 +80,10 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">头像</label>
                 <div class="col-sm-10">
-                  <button type="button" v-on:click="selectImage()" class="btn btn-white btn-default btn-round">
-                    <i class="ace-icon fa fa-upload"></i>
-                    上传头像
-                  </button>
-                  <input class="hidden" type="file" ref="file" v-on:change="uploadImage()" id="file-upload-input">
+                  <file v-bind:id="'image-upload'"
+                        v-bind:text="'上传头像'"
+                        v-bind:suffixs="['jpg', 'jpeg', 'png']"
+                        v-bind:after-upload="afterUpload"></file>
                   <div v-show="teacher.image" class="row">
                     <div class="col-md-4">
                       <img v-bind:src="teacher.image" class="img-responsive">
@@ -124,8 +123,9 @@
 
 <script>
   import Pagination from "../../components/pagination";
+  import File from "../../components/file";
   export default {
-    components: {Pagination},
+    components: {Pagination, File},
     name: "business-teacher",
     data: function() {
       return {
@@ -244,6 +244,12 @@
 
       selectImage () {
         $("#file-upload-input").trigger("click");
+      },
+
+      afterUpload(resp) {
+        let _this = this;
+        let image = resp.content;
+        _this.teacher.image = image;
       },
 
       /**
