12ba4ce324953b2a8f44e1c378125dcef3c93437
黎立健 <wb-llj614947@alibaba-inc.com>
2020-07-07 20:23:38 +0800

    文件上传后返回值的处理

--- admin/src/views/admin/teacher.vue
+++ admin/src/views/admin/teacher.vue
@@ -250,7 +250,7 @@
 
       afterUpload(resp) {
         let _this = this;
-        let image = resp.content;
+        let image = resp.content.path;
         _this.teacher.image = image;
       },
 
