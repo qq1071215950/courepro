2864b91862ce9e9f67148d1ace50c630cc750e6b
黎立健 <wb-llj614947@alibaba-inc.com>
2020-07-08 15:00:59 +0800

    修复请求路径

--- business/src/main/java/com/course/business/controller/admin/CourseContentFileController.java
+++ business/src/main/java/com/course/business/controller/admin/CourseContentFileController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
 @RestController
-@RequestMapping("/courseContentFile")
+@RequestMapping("/course-content-file")
 public class CourseContentFileController {
 
     private static final Logger LOG = LoggerFactory.getLogger(CourseContentFileController.class);
